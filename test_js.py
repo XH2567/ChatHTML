3c63fb52fd0463b1e6abc590e9793c8d0bbfa003
SORGY <sorm1y@qq.com>
2026-04-20 11:00:30 +0800

    删除原python文件

--- test_js.py
+++
@@ -1,7 +0,0 @@
-import re
-with open('/home/xings/LatexML/webapp/views.py', 'r') as f:
-    content = f.read()
-
-script = re.search(r'<script>(.*?)</script>', content, re.DOTALL)
-if script:
-    print(script.group(1)[:100])
